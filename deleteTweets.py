a9feeeb98670795963f7ba1c86570b858ba953ba
Preet Mishra <ipreetmishra@gmail.com>
2020-04-28 23:35:22 +0530

    Tidy up deleteTweets.py.
    * Rename screenName to screen_name.
    * Remove spaces for keyword arguments.
    * Add new line after import statements.
    * Tidy up the prompt message.

--- deleteTweets.py
+++ deleteTweets.py
@@ -1,7 +1,8 @@
 import os
 import tweepy
 
-# replace these with your keys and secrets
+
+# Replace these with your keys and secrets.
 CONSUMER_KEY = os.environ.get('TWITTER_CONSUMER_KEY')
 CONSUMER_SECRET = os.environ.get('TWITTER_CONSUMER_SECRET')
 ACCESS_KEY = os.environ.get('TWITTER_ACCESS_KEY')
@@ -11,10 +12,10 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-screenName = input('Enter your screen name without the hashtag: ')
-user = api.get_user(screen_name = screenName)
+screen_name = input('Enter your screen name (without the hashtag): ')
+user = api.get_user(screen_name=screen_name)
 
-tweets = api.user_timeline(id = user.id, include_rts = True)
+tweets = api.user_timeline(id=user.id, include_rts=True)
 for tweet in tweets:
     print('Deleting tweets...')
     api.destroy_status(tweet.id)
